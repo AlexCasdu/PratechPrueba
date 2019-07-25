9183b6fcacc99689850be674fca5403a9b810eac
AlexCasdu <42116081+AlexCasdu@users.noreply.github.com>
2019-07-25 15:02:15 -0500

    User model & authentication

---
+++ routes/api/users.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../../models/User');
+
+// @route    POST api/users
+// @desc     Register user
+// @access   Public
+router.post(
+  '/',
+  [
+    check('name', 'El nombre es requerrido')
+      .not()
+      .isEmpty(),
+    check('email', 'Por favor ingrese un email valido').isEmail(),
+    check(
+      'password',
+      'Por favor ingrese una contraseña de 6 o más caracteres'
+    ).isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'El usuario ya existe' }] });
+      }
+
+      /*const avatar = gravatar.url(email,{
+          s: '200',
+          r: 'pg',
+          d: 'mm'
+      })*/
+
+      user = new User({
+        name,
+        email,
+        password
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
+
+module.exports = router;
